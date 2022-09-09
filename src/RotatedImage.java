eda9c80205359c922e36cfb4767aefa9db732bdc
Ramin Kosfeld <r.kosfeld@tu-bs.de>
2022-09-10 00:31:34 +0200

    initial commit (halfway done with project though)

---
+++ src/RotatedImage.java
@@ -0,0 +1,10 @@
+import javafx.scene.image.Image;
+
+public class RotatedImage extends Image {
+    public RotatedImage(String url) {
+        super(url, true);
+    }
+    public int getRotation() {
+        
+    }
+}
